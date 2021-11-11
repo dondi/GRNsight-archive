f9960257565a18d1162a87ce9697e03e2147f746
Ahmad Mersaghian <75926755+ahmad00m@users.noreply.github.com>
2021-11-11 14:29:06 -0800

    Updated the more sophisticated code for selecting systematic names

--- documents/scripts/duplicate_expression_remover/standardname.py
+++ documents/scripts/duplicate_expression_remover/standardname.py
@@ -1,16 +1,20 @@
+import re
 stem_lines = []
-with open("/Users/ahmad00.m/Desktop/bioinformatics research 2021/cleaning file/wt_stem.txt") as f:
+with open("address to the file that you want to select the systematic names") as f:
     data = f.readlines()
     for i in data:
         stem_lines.append(i.split())
         
+r = re.compile("Y[A-P][LR][0-9][0-9][0-9][WwCc](-[A-Z])?")
+current_line_index = 0
 newList = []
 for comparison_line in stem_lines:
-    systemname = comparison_line[0]
-    if systemname[0] == "Y":
-        newList.append(comparison_line)
+    for match in r.finditer(comparison_line[0]):
+        gene = match.group()
+        if gene in comparison_line:
+            newList.append(comparison_line)
 
-with open("/Users/ahmad00.m/Desktop/bioinformatics research 2021/cleaning file/systematic_names.txt", "w") as textfile:
+with open("address to the file you want data to be written to", "w") as textfile:
 
     for lists in newList:
         fields = '\t'.join(lists)
